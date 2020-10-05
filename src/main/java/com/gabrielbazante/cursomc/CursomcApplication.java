5b90884938e2c7feaea1d6570fbc6835f673136d
Gabriel Bazante <gabrielbazante7@gmail.com>
2020-10-05 14:51:54 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/gabrielbazante/cursomc/CursomcApplication.java
+++ src/main/java/com/gabrielbazante/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.gabrielbazante.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.gabrielbazante.cursomc.domain.Cidade;
 import com.gabrielbazante.cursomc.domain.Cliente;
 import com.gabrielbazante.cursomc.domain.Endereco;
 import com.gabrielbazante.cursomc.domain.Estado;
+import com.gabrielbazante.cursomc.domain.Pagamento;
+import com.gabrielbazante.cursomc.domain.PagamentoComBoleto;
+import com.gabrielbazante.cursomc.domain.PagamentoComCartao;
+import com.gabrielbazante.cursomc.domain.Pedido;
 import com.gabrielbazante.cursomc.domain.Produto;
+import com.gabrielbazante.cursomc.domain.enums.EstadoPagamento;
 import com.gabrielbazante.cursomc.domain.enums.TipoCliente;
 import com.gabrielbazante.cursomc.repositories.CategoriaRepository;
 import com.gabrielbazante.cursomc.repositories.CidadeRepository;
 import com.gabrielbazante.cursomc.repositories.ClienteRepository;
 import com.gabrielbazante.cursomc.repositories.EnderecoRepository;
 import com.gabrielbazante.cursomc.repositories.EstadoRepository;
+import com.gabrielbazante.cursomc.repositories.PagamentoRepository;
+import com.gabrielbazante.cursomc.repositories.PedidoRepository;
 import com.gabrielbazante.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -36,6 +44,10 @@ public class CursomcApplication implements CommandLineRunner {
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -87,6 +99,21 @@ public class CursomcApplication implements CommandLineRunner {
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1,e2));
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		
+		Pedido ped1 = new Pedido(null, sdf.parse("05/10/2020 12:00"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("05/10/2020 13:00"), cli1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("10/10/2020 12:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1,ped2));
+		
+		pedidoRepository.saveAll(Arrays.asList(ped1,ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1,pagto2));
 		
 	}
 
--- src/main/java/com/gabrielbazante/cursomc/domain/Cliente.java
+++ src/main/java/com/gabrielbazante/cursomc/domain/Cliente.java
@@ -38,6 +38,9 @@ public class Cliente  implements Serializable{
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
+	@OneToMany(mappedBy="cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
+	
 	public Cliente() {
 		
 	}
@@ -86,6 +89,35 @@ public class Cliente  implements Serializable{
 	public TipoCliente getTipo() {
 		return TipoCliente.toEnum(tipo);
 	}
+	
+	public void setTipo(TipoCliente tipo) {
+		this.tipo = tipo.getCod();
+	}
+
+	public List<Endereco> getEnderecos() {
+		return enderecos;
+	}
+
+	public void setEnderecos(List<Endereco> enderecos) {
+		this.enderecos = enderecos;
+	}
+
+	public Set<String> getTelefones() {
+		return telefones;
+	}
+
+	public void setTelefones(Set<String> telefones) {
+		this.telefones = telefones;
+	}
+
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+	
 
 	@Override
 	public int hashCode() {
@@ -112,25 +144,6 @@ public class Cliente  implements Serializable{
 		return true;
 	}
 
-	public void setTipo(TipoCliente tipo) {
-		this.tipo = tipo.getCod();
-	}
 
-	public List<Endereco> getEnderecos() {
-		return enderecos;
-	}
-
-	public void setEnderecos(List<Endereco> enderecos) {
-		this.enderecos = enderecos;
-	}
-
-	public Set<String> getTelefones() {
-		return telefones;
-	}
-
-	public void setTelefones(Set<String> telefones) {
-		this.telefones = telefones;
-	}
-	
 	
 }
